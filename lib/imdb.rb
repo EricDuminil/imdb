88e5026a96f84baa80627a465f34dfcedd9dd436
Eric Duminil <eric.duminil@gmail.com>
2023-01-02 15:01:11 +0100

    Updated URL and header, to avoid 308

--- lib/imdb.rb
+++ lib/imdb.rb
@@ -10,7 +10,7 @@ module Imdb
   # www.imdb.com returns 'Die Verurteilten' for 'The Shawshank Redemption' in Germany.
   # This header is needed to disable localizing.
   # use_it with open-uri's open or `curl`
-  HTTP_HEADER = { 'Accept-Language' => 'en-US;en' }.freeze
+  HTTP_HEADER = { 'Accept-Language' => 'en-US;en', 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36', "Referer" => "https://www.imdb.com/"}.freeze
   HTTP_PROTOCOL = 'https'.freeze
 end
 
